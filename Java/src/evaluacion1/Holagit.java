c90464c514895b7df28008aaf1516b91998b288c
selennaa <45819762+selennaa@users.noreply.github.com>
2018-12-12 13:36:37 +0100

    Version 0.0

---
+++ Java/src/evaluacion1/Holagit.java
@@ -0,0 +1,10 @@
+package evaluacion1;
+
+public class Holagit {
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+		System.out.println("Holagit");
+	}
+
+}
